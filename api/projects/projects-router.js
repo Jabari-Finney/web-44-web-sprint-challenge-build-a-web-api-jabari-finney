89bba7e9431e00c84b098dd19bae2199b5af029e
Jabari-Finney <jabaritf@gmail.com>
2021-08-06 19:59:31 -0400

    mvp still failing

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,19 +1,69 @@
-const Adopter = require('./projects-model');
-const express = require('express');
+const express = require("express");
+
+const Projects = require("./projects-model");
+
+const {
+    validateProjID,
+    validateProjBody,
+    validateProjUpdate,
+} = require("./projects-middleware");
+
 const router = express.Router();
 
-// Write your "projects" router here!
- router.get('/api/projects', (req, res) => {
-    Adopter.find(req.query)
-        .then(adopters => {
-            res.status(200).json(adopters);
-        })
-        .catch(error =>{
-            console.log(error);
-            res.status(500).json([{}])
-        })
- })
+router.get("/", (req, res, next) => {
+  Projects.get()
+    .then((projects) => {
+      if (!projects) {
+        res.send([]);
+      } else {
+        res.status(200).json(projects);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err.message,
+      });
+    });
+});
 
+router.get("/:id", validateProjID, (req, res, next) => {
+  res.status(200).json(req.project);
+});
 
+router.post("/", validateProjBody, (req, res, next) => {
+  Projects.insert(req.body)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch(next);
+});
 
- module.exports = router
+router.put("/:id", validateProjID, validateProjUpdate, (req, res, next) => {
+  Projects.update(req.params.id, req.body)
+    .then((updated) => {
+      res.status(200).json(updated);
+    })
+    .catch(next);
+});
+
+router.delete("/:id", validateProjID, (req, res, next) => {
+  Projects.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: "project deleted" });
+    })
+    .catch(next);
+});
+
+router.get("/:id/actions", validateProjID, (req, res, next) => {
+  Projects.getProjectActions(req.params.id)
+    .then((actions) => {
+      if (!actions) {
+        res.send([]);
+      } else {
+        res.status(200).json(actions);
+      }
+    })
+    .catch(next);
+});
+
+module.exports = router;
